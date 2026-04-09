d789177367a7b7a3141e9da9d4a2f05aced4e83b
Erfi Anugrah <erfi.anugrah@gmail.com>
2026-04-09 18:18:03 +0200

    v0.5.7

--- internal/api/server.go
+++ internal/api/server.go
@@ -87,13 +87,13 @@ func NewServer(deps Deps) *Server {
 	}, func(ctx context.Context, input *struct{}) (*struct {
 		Body struct {
 			Status  string `json:"status" example:"healthy"`
-			Version string `json:"version" example:"0.5.6"`
+			Version string `json:"version" example:"0.5.7"`
 		}
 	}, error) {
 		resp := &struct {
 			Body struct {
 				Status  string `json:"status" example:"healthy"`
-				Version string `json:"version" example:"0.5.6"`
+				Version string `json:"version" example:"0.5.7"`
 			}
 		}{}
 		resp.Body.Status = "healthy"
