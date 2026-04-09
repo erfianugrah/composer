785b04e7d63014a26c5efd3c0a869c7824c1ea69
Erfi Anugrah <erfi.anugrah@gmail.com>
2026-04-09 17:15:29 +0200

    v0.5.3: parallel multi-arch Docker builds

--- internal/api/server.go
+++ internal/api/server.go
@@ -86,13 +86,13 @@ func NewServer(deps Deps) *Server {
 	}, func(ctx context.Context, input *struct{}) (*struct {
 		Body struct {
 			Status  string `json:"status" example:"healthy"`
-			Version string `json:"version" example:"0.5.2"`
+			Version string `json:"version" example:"0.5.3"`
 		}
 	}, error) {
 		resp := &struct {
 			Body struct {
 				Status  string `json:"status" example:"healthy"`
-				Version string `json:"version" example:"0.5.2"`
+				Version string `json:"version" example:"0.5.3"`
 			}
 		}{}
 		resp.Body.Status = "healthy"
